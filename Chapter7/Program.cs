c6b5758f90f6d0b71869f048dfe25b4c02bf3ae0
りんざわ <31095ojs.ac.jp>
2020-10-15 15:15:48 +0900

    辞書登録

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -49,14 +49,98 @@ namespace Chapter7 {
             //var md = new MonthDay(8, 4);
             //var s = dict[md];
             //Console.WriteLine(s);
+
+
+
+
+
+
+
+
+
+
+
+            //var lines = File.ReadAllLines("sample.txt");
+            //var we = new WordsExtractor(lines);
+            //foreach (var word in we.Extract()) {
+            //    Console.WriteLine(word);
+            //}
             #endregion
-            var lines = File.ReadAllLines("sample.txt");
-            var we = new WordsExtractor(lines);
-            foreach (var word in we.Extract()) {
-                Console.WriteLine(word);
+            
+            Console.WriteLine("**********************");
+            Console.WriteLine("* 辞書登録プログラム *");
+            Console.WriteLine("**********************");
+            Console.WriteLine();
+            //辞書
+            var dict = new Dictionary<string, List<string>>();
+
+
+            while (true) {
+                Console.WriteLine("1.登録　2.内容を表示　3.終了");
+                Console.Write(">");
+                int select = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                
+
+
+                switch (select) {
+                    case 1:
+                        Console.Write("KEYを入力:");
+                        var key = Console.ReadLine();
+                        Console.Write("VALUEを入力:");
+                        var value = Console.ReadLine();
+                        if (dict.ContainsKey(key)) {
+                            dict[key].Add(value);
+                        } else {
+                            dict[key] = new List<string> { value };
+                        }
+                        Console.WriteLine("登録しました！");
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        foreach (var item in dict) {
+                            foreach (var nene in item.Value) {
+                                Console.WriteLine($"{item.Key} : { nene}");
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+
+
+
+
+        static public void DuplicateKeySample() {
+            // ディクショナリの初期化
+            var dict = new Dictionary<string, List<string>>() {
+               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+            };
+
+            // ディクショナリに追加
+            var key = "EC";
+            var value = "電子商取引";
+            if (dict.ContainsKey(key)) {
+                dict[key].Add(value);
+            } else {
+                dict[key] = new List<string> { value };
             }
 
-
+            // ディクショナリの内容を列挙
+            foreach (var item in dict) {
+                foreach (var term in item.Value) {
+                    Console.WriteLine("{0} : {1}", item.Key, term);
+                }
+            }
         }
+
+        
     }
 }
