feac01cdd61cf78660795dd21cb1ccf832196267
RinOkuzawa <31095@ojs.ac.jp>
2020-11-13 16:03:05 +0900

    ないしてたかわからない

--- SendMailApp/ConfigWindow.xaml.cs
+++ SendMailApp/ConfigWindow.xaml.cs
@@ -8,23 +8,26 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
 
 namespace SendMailApp {
     /// <summary>
     /// ConfigWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class ConfigWindow : Window {
+        
 
         public ConfigWindow() {
             InitializeComponent();
         }
 
         private void btDefault_Click(object sender, RoutedEventArgs e) {
-            Config cf =( Config.GetInstance()).GetDefaultStatus();
+            Config cf = (Config.GetInstance()).GetDefaultStatus();
             tbSmtp.Text = cf.Smtp;
             tbSender.Text = cf.MailAddress;
             tbUserName.Text = cf.MailAddress;
@@ -37,17 +40,31 @@ namespace SendMailApp {
 
         //キャンセルボタン
         private void btCancel_Click(object sender, RoutedEventArgs e) {
-            this.Close();
-        }
+            if (DataCheck()) {
+                if (ChangeTextJg()) {
+                    this.Close();
+                } 
+            } else if(CancelSaveStatus() == true){
+                Message();
+            } else if(CancelSaveStatus() == false){
+                setStatus();
+            } else {
+                this.Close();
+            }
+        } 
 
-        //適用(更新)
+        //適用(更新) 
         private void btApply_Click(object sender, RoutedEventArgs e) {
-            CancelSaveStatus();
+            if (CancelSaveStatus() == true) {
+                Message();
+            } else if (CancelSaveStatus() == false) {
+                setStatus();
+            }
         }
 
         //OKボタン
         private void btOk_Click(object sender, RoutedEventArgs e) {
-            btApply_Click(sender,e);  //更新処理を呼び出す
+            btApply_Click(sender, e);  //更新処理を呼び出す
             this.Close();
         }
 
@@ -63,26 +80,55 @@ namespace SendMailApp {
         }
 
         //未入力項目チェックメソッド
-        public static void CancelSaveStatus() {
+        public static bool CancelSaveStatus() {
             ConfigWindow cw = new ConfigWindow();
-            if (cw.tbSmtp.Text == "" || cw.tbUserName.Text == "" || cw.tbSender.Text == "" || cw.tbPort.Text == "" || cw.tbPassWord.Password == "") {
-                MessageBox.Show("入力されていない項目があります");
+            if (cw.tbSmtp.Text == "" || cw.tbUserName.Text == "" || cw.tbSender.Text == "" || int.Parse(cw.tbPort.Text) == 0|| cw.tbPassWord.Password == "") {
+                return true;
             } else {
-                (Config.GetInstance()).UpdateStatus(
+                return false;
+            }
+
+        }
+
+        //メッセージボックスメソッド
+            public static bool ChangeTextJg() {
+            ConfigWindow cw = new ConfigWindow();
+            MessageBoxResult result = MessageBox.Show("変更されていますがキャンセルしてよろしいですか？", "警告", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK) {
+                return true;
+            } else  {
+                return false;
+            }
+        }
+        
+        public static void Message() {
+            MessageBox.Show("入力されていない項目があります");
+        }
+        
+
+        public void setStatus() {
+            ConfigWindow cw = new ConfigWindow();
+            (Config.GetInstance()).UpdateStatus(
                 cw.tbSmtp.Text,
                 cw.tbSender.Text,
                 cw.tbPassWord.Password,
                 int.Parse(cw.tbPort.Text),
                 cw.cbSsl.IsChecked ?? false
                 );
-            }
-
         }
 
-
-        public static void DataCheck() {
+        //データが変更されているかの判定
+        public static bool DataCheck() {
             ConfigWindow cw = new ConfigWindow();
+            Config cf = Config.GetInstance();
+
             
+            if (cf.Smtp != cw.tbSmtp.Text || cf.MailAddress != cw.tbSender.Text || cf.PassWord != cw.tbPassWord.Password || cf.Port != int.Parse(cw.tbPort.Text) || cf.Ssl != cw.cbSsl.IsChecked || cf.MailAddress != cw.tbUserName.Text) {
+                return true;
+            } else {
+                return false;
+            }
+               
         }
     }
 }
